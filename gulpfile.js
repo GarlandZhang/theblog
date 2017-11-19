da8dee33664d6c05f9823e5f576cc445be0faafe
GarlandZhang <garland._zhang@hotmail.com>
2017-11-19 18:50:30 -0500

    redesigned structure of project

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static Server + watching scss/html files
+gulp.task('start', function() {
+    browserSync.init({
+        proxy: "localhost:3000"
+    });
+    gulp.watch(["routes/*.html", "views/*.ejs"]).on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+
+gulp.task('default', ['start']);
