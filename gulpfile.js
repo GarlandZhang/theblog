e8cee2a896057c435ffcb70ce21eea5c75976eb1
GarlandZhang <garland._zhang@hotmail.com>
2018-01-20 23:41:02 -0500

    JUST APPLIED TO 50 JOBS

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var browserSync = require('browser-sync').create();
 // Static Server + watching scss/html files
 gulp.task('start', function() {
     browserSync.init({
-        proxy: "localhost:3000"
+        proxy: "localhost:3000",
+		port: 3001
     });
     gulp.watch(["routes/*.html", "views/*.ejs"]).on('change', browserSync.reload);
 });
